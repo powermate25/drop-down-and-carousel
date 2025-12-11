5f4ac7d6bbb31f6d65e26f1d9fbd69886bd2847f
powermate25 <218421152+powermate25@users.noreply.github.com>
2025-12-11 13:15:48 +0000

    Initial commit

---
+++ src/app/module1-dashboard-ui.js
@@ -0,0 +1,23 @@
+console.log("Working!")
+const clog = console.log
+/// Date Display logic start
+
+const dateDiv = document.querySelector(".date-now")
+const timeDiv = document.querySelector(".time-now")
+
+const displayTimeDate = setInterval(
+    () => {
+        let timeNow = new Date()
+        let currentDate = timeNow.toDateString()  + ", "
+        let currentTime = timeNow.getHours() + ":" + timeNow.getMinutes() + ":" + timeNow.getSeconds()
+        dateDiv.textContent = currentDate
+        timeDiv.textContent = currentTime
+
+   // clog("tick")
+    }, 1000)
+
+/// Date Display logic end 
+
+
+/// EXPORTING
+export { displayTimeDate }
